f6ada4a8ac4f268273484857d013d69d119ece76
laulampaul <46311776+laulampaul@users.noreply.github.com>
2021-04-19 14:26:26 +0800

    Update test_sgnet_ps200.sh

--- script/test_sgnet_ps200.sh
+++ script/test_sgnet_ps200.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-python -u test.py --pretrained_model checkpoints/checkpoints-tenet2-dn-matx6-6-6-64-2-rrdb-ps200 \
---scale 2 --bias --model tenet2 --denoise --sigma 0 --postname ps200 --crop_scale 8 --show_info \
+python test.py --pretrained_model your/save/path --scale 1 --model sgnet --denoise --sigma 0 --postname ps200 --crop_scale 8 --show_info \
 --test_path /cache/liulin/pixelshift/PixelShift200_val_crop --save_path output
 
 
