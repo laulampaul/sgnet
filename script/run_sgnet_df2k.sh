0db57aa7b0ee245a7130a7a39d2bd00abbe2cb78
laulampaul <46311776+laulampaul@users.noreply.github.com>
2021-04-19 14:23:21 +0800

    Update run_sgnet_df2k.sh

--- script/run_sgnet_df2k.sh
+++ script/run_sgnet_df2k.sh
@@ -3,8 +3,8 @@ python -u train.py\
 	--train_list datasets/train_df2k.txt --valid_list datasets/valid_df2k.txt\
     --model sgnet --bias --scale 1  --get2label\
     --denoise --max_noise 0.078 --min_noise 0.00
-# noise 15: 0.06
-# noise 10: 0.04
-# noise 5: 0.02
+# max_noise 15: 0.06
+# max_noise 10: 0.04
+# max_noise 5: 0.02
 # if run out of memory, lower batch_size down
 
