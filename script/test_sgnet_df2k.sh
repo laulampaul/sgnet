8678df44f1b6cedb4951d3063dd3eb830d366a30
laulampaul <46311776+laulampaul@users.noreply.github.com>
2021-04-19 14:25:24 +0800

    Update test_sgnet_df2k.sh

--- script/test_sgnet_df2k.sh
+++ script/test_sgnet_df2k.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-python -u test.py --pretrained_model checkpoints/tenet2-dn-df2k.path --model tenet2 --scale 2 --bias --denoise --sigma 0 \
---test_path ../TENet_sim_test/input/urban100 --save_path output --crop_scale 8 --postname df2k --show_info
+python test.py --pretrained_model your/save/path --model sgnet --scale 1 --bias --denoise --sigma 0 \
+--test_path ../TENet_sim_test/input/urban100 --save_path output --crop_scale 8 --postname df2k
 
 
 # if Run out of CUDA memory, just set --crop_scale 4 (or higher)
-# remember to change sigma according to the noise level of the current image
 # remember to change sigma according to the noise level of the current image
